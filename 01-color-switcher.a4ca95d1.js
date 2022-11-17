0e625fec2dd392b492f67d2fa92a9b5aef620dbf
OlgaPikulska <108013038+OlgaPikulska@users.noreply.github.com>
2022-11-17 11:12:41 +0000

    Deploying to gh-pages from @ OlgaPikulska/goit-js-hw-09@81f870a8326af9aafc0b0933b7d044d8e9ae26bd 🚀

---
+++ 01-color-switcher.a4ca95d1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d=null;t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled",""),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.a4ca95d1.js.map
