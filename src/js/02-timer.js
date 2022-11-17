b7836703308729a331907cc38445ece3d6bd7df5
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-17 13:59:28 +0100

    task 2 initial commit

---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+flatpickr("#datetime-picker", {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+  },
+});
+
+const btnStart = document.querySelector("button[data-start]");
