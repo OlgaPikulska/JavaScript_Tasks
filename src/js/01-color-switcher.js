81f870a8326af9aafc0b0933b7d044d8e9ae26bd
Olga Pikulska <olga.pikulska.ld@gmail.com>
2022-11-17 12:12:18 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = null
+
+startBtn.addEventListener("click", () => {
+    startBtn.setAttribute("disabled", "");
+    stopBtn.removeAttribute("disabled");
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+})
+
+stopBtn.addEventListener("click", () => {
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", "");
+    clearInterval(timerId);
+})
+
+
+
+
+
+
+
+//window.addEventListener("load", changeColor);
